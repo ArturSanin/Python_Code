3630798718abf4a5f7365bd433e620d483170a98
Artur Sanin <artur.sanin@web.de>
2024-09-22 06:55:44 +0200

    Added code for the Wolfram Challenge One-Finger Distance.

---
+++ Small_Projects/Wolfram_Challenges/one-finger_distance/one_finger_distance.py
@@ -0,0 +1,27 @@
+"""
+    Description: This is my attempted on the Wolfram Challenge One-Finger Distance, that you can find under the
+    following link: https://challenges.wolframcloud.com/challenge/one-finger-distance
+"""
+
+
+def one_finger_distance(word):
+    """
+
+    :param word:
+    :return:
+    """
+    lowercase_letters = [letter for letter in "abcdefghijklmnopqrstuvwxyz"]
+    word_length = len(word)
+    distance = 0
+    for character in word:
+        if character not in lowercase_letters:
+            raise ValueError("Your input must be a word consisting of small letters.")
+    for i in range(word_length - 1):
+        letter_index_1 = lowercase_letters.index(word[i])
+        letter_index_2 = lowercase_letters.index(word[i + 1])
+        absolut_value_index_differenz = abs(letter_index_2 - letter_index_1)
+        if absolut_value_index_differenz <= 1:
+            distance = distance + 0
+        else:
+            distance = distance + absolut_value_index_differenz - 1
+    return distance
---
+++ Small_Projects/Wolfram_Challenges/one-finger_distance/one_finger_distance_test.py
@@ -0,0 +1,24 @@
+from one_finger_distance import one_finger_distance
+
+
+test_string_1 = "boost"
+test_string_2 = "addage"
+test_string_3 = "adagio"
+test_string_4 = "abcdef"
+test_string_5 = "internationalization"
+# test_string_6 = "Hallo, how are you?"
+# test_string_7 = "123"
+# test_string_8 = "One Two"
+# test_string_9 = "one,two,three"
+# test_string_10 = "#hey"
+
+print(one_finger_distance(test_string_1))
+print(one_finger_distance(test_string_2))
+print(one_finger_distance(test_string_3))
+print(one_finger_distance(test_string_4))
+print(one_finger_distance(test_string_5))
+# print(one_finger_distance(test_string_6))
+# print(one_finger_distance(test_string_7))
+# print(one_finger_distance(test_string_8))
+# print(one_finger_distance(test_string_9))
+# print(one_finger_distance(test_string_10))
